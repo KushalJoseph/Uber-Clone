a60c16a8167cabf2a15e42d9d311b3f61e157a55
Kushal Joseph <65895442+KushalJoseph@users.noreply.github.com>
2022-01-23 21:46:41 +0530

    Update ShowRiderInfo.java

--- app/src/main/java/com/example/uber/ShowRiderInfo.java
+++ app/src/main/java/com/example/uber/ShowRiderInfo.java
@@ -36,21 +36,21 @@ public class ShowRiderInfo extends AppCompatActivity {
     TextView destinationText;
     TextView distanceText;
     String username;
-    Location riderLocLocation=new Location(LocationManager.GPS_PROVIDER);
-    Location driverLoc=new Location(LocationManager.GPS_PROVIDER);
+    Location riderLocLocation = new Location(LocationManager.GPS_PROVIDER);
+    Location driverLoc = new Location(LocationManager.GPS_PROVIDER);
 
     public void acceptRequest(View view)
     {
         ParseQuery<ParseObject>query=ParseQuery.getQuery("Requests");
-        query.whereEqualTo("username",username);
+        query.whereEqualTo("username", username);
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> objects, ParseException e) {
-                if(e==null&&objects.size()>0)
+                if(e == null && objects.size() > 0)
                 {
                     for(ParseObject object:objects)
                     {
-                        if(object.getBoolean("AcceptedOrNot")==true)
+                        if(object.getBoolean("AcceptedOrNot") == true)
                         {
                             Toast.makeText(ShowRiderInfo.this, "Sorry, this request was already accepted by another driver.", Toast.LENGTH_SHORT).show();
                         }
@@ -58,7 +58,7 @@ public class ShowRiderInfo extends AppCompatActivity {
                             object.put("driverUsername", ParseUser.getCurrentUser().getUsername());
                             object.put("AcceptedOrNot", true);
                             object.saveInBackground();
-                            ParseUser.getCurrentUser().put("currentDriverLocation",new ParseGeoPoint(driverLoc.getLatitude(),driverLoc.getLongitude()));
+                            ParseUser.getCurrentUser().put("currentDriverLocation", new ParseGeoPoint(driverLoc.getLatitude(),driverLoc.getLongitude()));
                             ParseUser.getCurrentUser().saveInBackground();
                             Toast.makeText(ShowRiderInfo.this, "Request accepted. Opening navigation...", Toast.LENGTH_SHORT).show();
                             Intent directionsIntent = new Intent(android.content.Intent.ACTION_VIEW,
@@ -77,35 +77,35 @@ public class ShowRiderInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_rider_info);
 
-        riderLocation=findViewById(R.id.riderLoc);
-        destinationText=findViewById(R.id.destination);
-        distanceText=findViewById(R.id.distance);
+        riderLocation = findViewById(R.id.riderLoc);
+        destinationText = findViewById(R.id.destination);
+        distanceText = findViewById(R.id.distance);
 
-        Intent intent=getIntent();
+        Intent intent = getIntent();
 
-        username=intent.getStringExtra("username");
-        String destination=intent.getStringExtra("destination");
-        LatLng riderLoc=new LatLng(intent.getDoubleExtra("riderLatitude",0),intent.getDoubleExtra("riderLongitude",0));
+        username = intent.getStringExtra("username");
+        String destination = intent.getStringExtra("destination");
+        LatLng riderLoc = new LatLng(intent.getDoubleExtra("riderLatitude", 0), intent.getDoubleExtra("riderLongitude", 0));
 
 
-        riderLocLocation.setLatitude(intent.getDoubleExtra("riderLatitude",0));
-        riderLocLocation.setLongitude(intent.getDoubleExtra("riderLongitude",0));
+        riderLocLocation.setLatitude(intent.getDoubleExtra("riderLatitude", 0));
+        riderLocLocation.setLongitude(intent.getDoubleExtra("riderLongitude", 0));
 
-        driverLoc.setLatitude(intent.getDoubleExtra("driverLatitude",0));
-        driverLoc.setLongitude(intent.getDoubleExtra("driverLongitude",0));
+        driverLoc.setLatitude(intent.getDoubleExtra("driverLatitude", 0));
+        driverLoc.setLongitude(intent.getDoubleExtra("driverLongitude", 0));
 
-        Double distance=(double)riderLocLocation.distanceTo(driverLoc);
-        destinationText.setText("Desired destination: "+destination);
-        distanceText.setText("Distance to Rider: "+Double.toString(maniDouble(distance))+" km.");
+        Double distance = (double) riderLocLocation.distanceTo(driverLoc);
+        destinationText.setText("Desired destination: " + destination);
+        distanceText.setText("Distance to Rider: " + Double.toString(maniDouble(distance)) + " km.");
 
-        Geocoder geocoder=new Geocoder(getApplicationContext(), Locale.getDefault());
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
         try
         {
-            List<Address>addresses=geocoder.getFromLocation(intent.getDoubleExtra("riderLatitude",0),
-                    intent.getDoubleExtra("riderLongitude",0),1);
-            if(addresses!=null && addresses.size()>0)
+            List<Address>addresses = geocoder.getFromLocation(intent.getDoubleExtra("riderLatitude", 0),
+                    intent.getDoubleExtra("riderLongitude", 0), 1);
+            if(addresses != null && addresses.size() > 0)
             {
-                if(addresses.get(0).getAddressLine(0)!=null)
+                if(addresses.get(0).getAddressLine(0) != null)
                 {
                     riderLocation.setText(addresses.get(0).getAddressLine(0));
                 }
@@ -121,13 +121,13 @@ public class ShowRiderInfo extends AppCompatActivity {
 
     public double maniDouble(double num)
     {
-        num=Math.round(num*10)/10;
+        num = Math.round(num * 10)/10;
         return num/1000;
     }
 
     public void cancel(View view)
     {
-        Intent intent=new Intent(getApplicationContext(), DriverMap.class);
+        Intent intent = new Intent(getApplicationContext(), DriverMap.class);
         startActivity(intent);
     }
 }
